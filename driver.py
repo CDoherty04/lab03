82b9c5066acea4bcbe532ccdf5fc6bf68db4c3c8
Charlie <charlieedoherty@gmail.com>
2024-02-13 10:09:31 -0600

    Add input file and finalize driver

--- driver.py
+++ driver.py
@@ -1,13 +1,24 @@
 """
+Author: Charlie Doherty
+KUID: 3115329
+Date: 2/13/24
+Lab: 03
+Last modified: 02/13/24
+Purpose: Create linked lists to simulate a browser history
 
-
+Run driver to execute the program and modify commands.txt to change the terminal's output
 """
 from executive import Executive
 
 
 def main():
-    pass
+    """Hands the process over to an Executive object"""
+
+    my_exec = Executive()
+    my_exec.run()
 
 
 if __name__ == "__main__":
+    """Call stack begins here"""
+
     main()
--- executive.py
+++ executive.py
@@ -1,5 +1,35 @@
 from history import History
 
 
+def get_commands():
+    """Takes user input for the input file and converts it into a list of commands"""
+
+    while True:
+        # Repeats the prompt until a valid file is given
+
+        try:
+            file_name = input("Name of the input file: ")
+
+            with open(file_name, "r") as file:
+                lines = file.readlines()
+            break
+
+        except FileNotFoundError:
+            print("Not a valid file, try again")
+
+    # Removes newlines
+    lines = [line.strip() for line in lines]
+    return lines
+
+
 class Executive:
-    pass
+    """The class that handles files and function calls"""
+
+    def __init__(self):
+        """Initializes the Exec object by getting file input"""
+
+        self._commands = get_commands()
+
+    def run(self):
+        """Acts as the main method"""
+        pass
