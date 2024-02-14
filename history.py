b1e229f64a08ba0e0435c813b0dcdb44beece25b
Charlie <charlieedoherty@gmail.com>
2024-02-14 15:50:45 -0600

    Implement back() and allow navigate_to() to remove URLs past the current pointer

--- history.py
+++ history.py
@@ -6,14 +6,18 @@ class History:
 
     def __init__(self):
         self.browser_history = LinkedList()
-        self.place = -1
+        self._place = -1
 
     def navigate_to(self, url):
         """The browser navigates to the given url"""
 
         # Insert a URL after the set place
-        self.browser_history.insert(self.place+1, url)
-        self.place += 1
+        self.browser_history.insert(self._place+1, url)
+        self._place += 1
+
+        # Remove all URLs past the inserted point
+        while self.browser_history.length() > self._place + 1:
+            self.browser_history.remove(self._place + 1)
 
     def forward(self):
         """If possible, the browser navigates forward in the history, otherwise it stays in place"""
@@ -23,16 +27,23 @@ class History:
     def back(self):
         """If possible, the browser navigates backwards in the history, otherwise it stays in place"""
 
-        pass
+        if self._place > 0:
+            self._place -= 1
 
     def history(self):
         """Returns a well formatted string with the current history"""
 
         content = "Oldest\n===========\n"
+        
+        # Add every URL
         for i in range(self.browser_history.length()):
-            if self.place == i:
+
+            # Place current pointer at appropriate URL
+            if self._place == i:
                 content += self.browser_history.get_entry(i) + "\t<==current\n"
             else:
                 content += self.browser_history.get_entry(i) + "\n"
+        
         content += "===========\nNewest\n"
+        
         return content
