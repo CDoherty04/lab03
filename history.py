da9f36e1b1b1e30633af49b4404a9df9425a2ec4
Charlie <charlieedoherty@gmail.com>
2024-02-13 21:57:45 -0600

    Format history.py

--- history.py
+++ history.py
@@ -2,4 +2,27 @@ from linkedlist import LinkedList
 
 
 class History:
-    pass
+    """A class that mimics the behavior of a web browser's back button, forward button, and address bar"""
+
+    def __init__(self):
+        self.browser_history = LinkedList()
+
+    def navigate_to(self, url):
+        """The browser navigates to the given url"""
+
+        pass
+
+    def forward(self):
+        """If possible, the browser navigates forward in the history, otherwise it stays in place"""
+
+        pass
+
+    def back(self):
+        """If possible, the browser navigates backwards in the history, otherwise it stays in place"""
+
+        pass
+
+    def history(self):
+        """Returns a well formatted string with the current history"""
+
+        pass
